4aae60a15b9a2ced5de3859c6a871c674b321a19
sinke237 <enowsinke@gmail.com>
2024-04-16 11:43:15 +0100

    fix: getting image name from the .yml file

--- environmentUpdate.sh
+++ environmentUpdate.sh
@@ -1,27 +1,65 @@
 #!/bin/bash
 
-IMAGE_TAG="latest"
+get_image_name(){
+  local image_name=$(grep "image:" docker-compose.yml | awk -F':' '{print $2}' | tr -d '[:space:]')
+  echo "$image_name"
+}
 
 update_environment(){
-    docker pull alpine:$IMAGE_TAG
+  local image_name=$(get_image_name)
+  docker pull "$image_name:$IMAGE_TAG"
 
-    # stop and remove the existing containers
-    docker-compose down
+  # stop and remove the existing containers
+  docker-compose down
 
-    docker-compose up -d
+  docker-compose up -d
+}
+
+# check to see if the image on the .yml file
+if ! grep -q "image: " docker-compose.yml; then
+  echo "ERROR: Image not found in the docker-compose.yml file."
+  exit 1
+fi
+
+# check if image is already running
+if ! docker image inspect "$(get_image_name):$IMAGE_TAG" >/dev/null 2>&1; then
+  echo "ERROR: Image '$(get_image_name)' is not running."
+  echo "Pulling '$(get_image_name):$IMAGE_TAG'..."
+  update_environment
+fi
+
+add_cron_job(){
+  (crontab -l ; echo "$1") | crontab -
+}
+
+remove_cron_job(){
+  crontab -l | grep -v "$1" | crontab -
+}
+
+schedule_day(){
+  add_cron_job "* * * * * ./environmentUpdate.sh"
+}
+
+schedule_night(){
+  add_cron_job "0 0 * * * ./environmentUpdate.sh"
 }
 
 monitor_changes(){
-    while true; do
-        latest_tag=$(docker image ls alpine --format "{{.Tag}}")
+  previous_tag=""
 
-        if [[ "$latest_tag" != "$IMAGE_TAG" ]]; then
-            echo "New image tag available: $latest_tag"
-            echo "Update environment..."
-            IMAGE_TAG="$latest_tag"
-            update_environment
-        fi
-        sleep 60 # sleep for a while before checking again...
+    while true; do
+      local repository=$(get_image_name)
+      local latest_tag=$(docker image ls "$repository" --format "{{.Tag}}")
+
+      if [[ "$latest_tag" != "$IMAGE_TAG" && "$latest_tag" != "$previous_tag" ]]; then
+          echo "New image tag available: $latest_tag"
+          echo "Update environment..."
+          IMAGE_TAG="$latest_tag"
+          update_environment
+          previous_tag="$latest_tag"
+      fi
+      sleep 60 # sleep for a while before checking again...
     done
 }
+schedule_day
 monitor_changes
